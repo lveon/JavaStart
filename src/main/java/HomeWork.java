4a565470d2f1aff4c6326b82f50963bb35885ca3
lveon <dzsodessa>
2020-01-30 22:49:01 +0200

    Homework

---
+++ src/main/java/HomeWork.java
@@ -0,0 +1,75 @@
+public class HomeWork {
+    public static void main(String[] args) {
+
+
+        /*Задача 1:
+        Есть числа a и b. Выяснить какое число больше и сообщить об этом*/
+
+       /* int i =  986;
+        int y =  986;
+
+        if (i > y) {
+            System.out.println("i bigger then y");
+
+        }else if(i < y) {
+            System.out.println("y bigger then i");
+        }else if (i == y) {
+            System.out.println("Победила дружба т.к. i = y");
+        }*/
+
+
+       /* Задача 2
+
+        Есть число a, необходимо опеределить четное ли число и сообщить об этом*/
+
+     /*  long a = 0;
+       byte b = 2 ;
+       long c ;
+
+          c = a % b;
+          if (c == 0) {
+              System.out.println("Вы ввели четное число");
+          }else
+              System.out.println("Вы ввели нечетное число");*/
+
+
+        // Задача3.
+        // Есть дробное число j. Вывести на экран его дробную часть и целую отдельно.
+
+
+     /*   double dDrobnoe = 15.7;
+
+
+       long cCeloe  = (long)  dDrobnoe;
+
+        System.out.println( "Целая часть = "+ cCeloe);
+
+        double oOstatok = dDrobnoe - cCeloe;
+        System.out.println( "Остаток = "+ oOstatok);*/
+
+
+        // Задача 4
+        //Есть числа а и b. Если сумма чисел больше или равна 15 то одно сообщение, если меньше другое, но если их разница
+        //отрицательная сообщить именно об этом
+
+       /* int a = -55;
+        int b = 1;
+        int c = a + b;
+
+        if (c >= 15) {
+            System.out.println("Cумма чисел больше или равна 15 ");
+
+        } else if (c < 15 && c >= 0) {
+            System.out.println("Сумма меньше 15 ");
+
+        } else {
+            System.out.println("Сообщаю, что разница отрицательная (с) Ваш кэп");
+
+        }
+*/
+
+    }
+}
+
+
+
